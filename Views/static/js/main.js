3f940be5954fe2456e306befd24101f950598f83
Ethan Steininger <esteininger21@gmail.com>
2018-10-26 16:04:46 -0400

    some office stuff

--- Views/static/js/main.js
+++ Views/static/js/main.js
@@ -80,6 +80,42 @@ function updateNav() {
   }
 
 
+  // // Create a new XMLHttpRequest.
+  // var request = new XMLHttpRequest();
+  //
+  // // Handle state changes for the request.
+  // request.onreadystatechange = function(response) {
+  //   if (request.readyState === 4) {
+  //     if (request.status === 200) {
+  //       // Parse the JSON
+  //       var jsonOptions = JSON.parse(request.responseText);
+  //
+  //       // Loop over the JSON array.
+  //       jsonOptions.forEach(function(item) {
+  //         // Create a new <option> element.
+  //         var option = document.createElement('option');
+  //         // Set the value using the item in the JSON array.
+  //         option.value = item;
+  //         // Add the <option> element to the <datalist>.
+  //         dataList.appendChild(option);
+  //       });
+  //
+  //       // Update the placeholder text.
+  //       input.placeholder = "e.g. datalist";
+  //     } else {
+  //       // An error occured :(
+  //       input.placeholder = "Couldn't load datalist options :(";
+  //     }
+  //   }
+  // };
+  //
+  // // Update the placeholder text.
+  // input.placeholder = "Loading options...";
+  //
+  // // Set up and make the request.
+  // request.open('GET', 'html-elements.json', true);
+  // request.send();
+
   // console.log(REPORT_ID, REPORT_IS_VALID)
 
 
